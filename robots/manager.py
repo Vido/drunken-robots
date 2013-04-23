987f849a7e0eb78719a46c3e3a8b8c5f25ba89d6
overlord <lucas.vido@ig.com.br>
2013-04-22 22:23:25 -0300

    New manager. Now you can run an individual bot.

--- robots/manager.py
+++ robots/manager.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 
@@ -6,26 +8,42 @@ basedir = os.path.dirname(abs_path)
 sys.path.append(basedir)
 
 MANEGED_ROBOTS = (
-	'PETR4',
+    'PETR4',
 )
 
-modules = map(__import__, MANEGED_ROBOTS)
+def run_robots(robots_list):
 
-#for robot  in MANEGED_ROBOTS:
-#	exec('import %s' % robot)
+    # modules = map(__import__, robots_list)
+    modules = []
 
-for module in modules:
-	try:
-		module.rules.should_it_run()
-	except Exception as e:
-		print "Robot %s skiped:\n %s" % (module.__name__, e)
-		continue
+    for robot in robots_list:
+        try:
+            module = __import__(robot, globals())
+            modules.append(module)
+        except Exception as e:
+            print "Robot %s has problems:\n %s" % (robot, e)
+            continue
 
-	try:
-		print "Running %s robot..." % module.__name__,
-		module.robot.make_your_magic()
-		print " OK."
-	except Exception as e:
-		print "Robot %s failed! \n %s" % (module.__name__, e)
+    for module in modules:
+        try:
+            module.rules.should_it_run()
+        except Exception as e:
+            print "Robot %s skiped:\n %s" % (module.__name__, e)
+            continue
 
+        try:
+            print "Running %s robot..." % module.__name__,
+            module.robot.make_your_magic()
+            print " OK."
+        except Exception as e:
+            print "Robot %s failed! \n %s" % (module.__name__, e)
+
+if __name__ == '__main__':
+
+    if len(sys.argv[1:]) == 0:
+        robots_list = MANEGED_ROBOTS
+    else:
+        robots_list = sys.argv[1:]
+
+    run_robots(robots_list)
 
