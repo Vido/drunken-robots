5d948bf1a7716ce70f0bbf8a741313b53364cb33
overlord <lucas.vido@ig.com.br>
2013-07-25 22:16:56 -0300

    Covariance formula

--- core/math/statistics.py
+++ core/math/statistics.py
@@ -1,10 +1,12 @@
 import math
 
+
 def mean(array):
     acc = 0.0
     for i in array:
         acc += i
-    return float(acc)/len(array)
+    return float(acc) / len(array)
+
 
 def variance(array):
     # Get a copy of the array
@@ -19,8 +21,22 @@ def variance(array):
     var = float(sum(a))/len(a)
     return var
 
+
 def standard_deviation(array):
     var = variance(array)
     std_dev = math.sqrt(var)
     return std_dev
+
+
+def covariance(array_x, array_y):
+    # http://mathworld.wolfram.com/Covariance.html
+    assert len(array_x) == len(array_y)
+    x_ = mean(array_x)
+    y_ = mean(array_y)
+
+    acc = 0.0
+    for xi, yi in zip(array_x, array_y):
+        acc += (xi - x_) * (yi - y_)
+
+    return float(acc) / len(array_x)
 
